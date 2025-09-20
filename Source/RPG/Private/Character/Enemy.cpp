523ce41bffe32c3fe5d94e6a0eae4b0a480cbc1b
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2025-09-20 23:27:10 +0530

    Post Processing

--- Source/RPG/Private/Character/Enemy.cpp
+++ Source/RPG/Private/Character/Enemy.cpp
@@ -3,12 +3,25 @@
 
 #include "Character/Enemy.h"
 
+#include "RPG/RPG.h"
+
+AEnemy::AEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+
+	Weapon->SetRenderCustomDepth(false);
 }
