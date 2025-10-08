fa7a374d4247cd8c0f332269fb374143c2a437a1
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2025-10-08 23:58:29 +0530

    Get All Asset tags

--- Source/RPG/Private/AbilitySystem/RPGAbilitySystemComponent.cpp
+++ Source/RPG/Private/AbilitySystem/RPGAbilitySystemComponent.cpp
@@ -11,5 +11,12 @@ void URPGAbilitySystemComponent::AbilityActorInfoSet()
 void URPGAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, FString("EffectApplied"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		// TODO: Broadcast the tag to the WidgetController
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, *Msg);
+	}
 }
