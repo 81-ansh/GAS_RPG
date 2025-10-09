1a17172f03ef223add9b79be93bf5796ea7f0088
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2025-10-09 21:42:53 +0530

    Retrieving Rows from Data Tables

--- Source/RPG/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/RPG/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -30,12 +30,14 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 		RPGAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
 
 	Cast<URPGAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
-		[](const FGameplayTagContainer& AssetTags)
+		[this](const FGameplayTagContainer& AssetTags)
 		{
 			for (const FGameplayTag& Tag : AssetTags)
 			{
 				const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
 				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, *Msg);
+
+				FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, Tag);
 			}
 		}
 	);
