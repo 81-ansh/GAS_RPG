26825967142ef6ab765c402843e6bbc2250b703e
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2025-10-23 15:11:23 +0530

    Native gameplay tags

--- Source/RPG/Private/AbilitySystem/RPGAbilitySystemComponent.cpp
+++ Source/RPG/Private/AbilitySystem/RPGAbilitySystemComponent.cpp
@@ -9,8 +9,6 @@ void URPGAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &URPGAbilitySystemComponent::EffectApplied);
 
-	const FRPGGameplayTags& GameplayTags = FRPGGameplayTags::Get();
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armour.ToString()));
 }
 
 void URPGAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
