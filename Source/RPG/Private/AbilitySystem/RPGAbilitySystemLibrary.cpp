9a1ba2db1a49b18948653ed4d21e58815956c126
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2025-10-24 15:28:45 +0530

    RPG Ability System Blueprint library

---
+++ Source/RPG/Private/AbilitySystem/RPGAbilitySystemLibrary.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/RPGAbilitySystemLibrary.h"
+#include "Player/MainPlayerState.h"
+#include "UI/WidgetController/RPGWidgetController.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/HUD/RPGHUD.h"
+
+UOverlayWidgetController* URPGAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (ARPGHUD* RPGHUD = Cast<ARPGHUD>(PC->GetHUD()))
+		{
+			AMainPlayerState* PS = PC->GetPlayerState<AMainPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS =	PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return RPGHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
---
+++ Source/RPG/Public/AbilitySystem/RPGAbilitySystemLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RPGAbilitySystemLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API URPGAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure, Category = "RPGAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
