cc2a3f3f723e36b4a5a027bfebfc9fb16757dc61
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2026-01-19 21:05:12 +0530

    Damage Meta Attribute

--- Source/RPG/Public/AbilitySystem/RPGAttributeSet.h
+++ Source/RPG/Public/AbilitySystem/RPGAttributeSet.h
@@ -143,6 +143,14 @@ public:
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
 	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(URPGAttributeSet, Mana);
+	
+	/*
+	 *	Meta Attributes
+	 */
+
+	UPROPERTY(BlueprintReadOnly, Category = "Meta Attributes")
+	FGameplayAttributeData IncomingDamage;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, IncomingDamage);
 
 	/*
 	 *  Primary Attributes Rep
