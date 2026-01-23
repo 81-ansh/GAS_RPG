15e7c86a0e6c90043e1f6a9dbaf7098893fb6dbd
Ansh Sharma <140145424+81-ansh@users.noreply.github.com>
2026-01-23 22:08:34 +0530

    Struct Ops Type Traits

--- Source/RPG/Public/RPGAbilityTypes.h
+++ Source/RPG/Public/RPGAbilityTypes.h
@@ -19,7 +19,20 @@ public:
 	/* Returns the actual struct used for serialization, subclass must override this! */
 	virtual UScriptStruct* GetScriptStruct() const
 	{
-		return FGameplayEffectContext::StaticStruct();
+		return StaticStruct();
+	}
+
+	/* Creates a copy of this context, used to duplicate for later modifications */
+	virtual FRPGGameplayEffectContext* Duplicate() const
+	{
+		FRPGGameplayEffectContext* NewContext = new FRPGGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			//Does a deep copy of hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
 	}
 
 	/* Custom serialization, subclass must override this */
@@ -32,6 +45,14 @@ protected:
 
 	UPROPERTY()
 	bool bIsCriticalHit = false;
+};
 
-	
+template<>
+struct TStructOpsTypeTraits<FRPGGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FRPGGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
