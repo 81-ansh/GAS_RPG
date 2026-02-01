1fdeb414612b867c1545dd07171d85b2c01197d6
Ansh Sharma <anshjangra15@gmail.com>
2026-02-01 22:35:55 +0530

    Causing Melee Damage

--- Source/RPG/Private/AbilitySystem/RPGAttributeSet.cpp
+++ Source/RPG/Private/AbilitySystem/RPGAttributeSet.cpp
@@ -184,6 +184,11 @@ void URPGAttributeSet::ShowFloatingText(const FEffectProperties& Props, float Da
 		if (AMainPlayerController* PC = Cast<AMainPlayerController>(Props.SourceCharacter->Controller))
 		{
 			PC->ShowDamageNumber(Damage, Props.TargetCharacter, bBlockedHit, bCriticalHit);
+			return;
+		}
+		if (AMainPlayerController* PC = Cast<AMainPlayerController>(Props.TargetCharacter->Controller))
+		{
+			PC->ShowDamageNumber(Damage, Props.TargetCharacter, bBlockedHit, bCriticalHit);
 		}
 	}
 }
--- Source/RPG/Public/AbilitySystem/Abilities/RPGDamageGameplayAbility.h
+++ Source/RPG/Public/AbilitySystem/Abilities/RPGDamageGameplayAbility.h
@@ -14,6 +14,11 @@ class RPG_API URPGDamageGameplayAbility : public URPGGameplayAbility
 {
 	GENERATED_BODY()
 	
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	void CauseDamage(AActor* TargetActor);
+	
 protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
