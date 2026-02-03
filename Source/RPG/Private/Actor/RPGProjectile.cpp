9b6e8b1147b320a24873eedc001f1c78523714f1
Ansh Sharma <anshjangra15@gmail.com>
2026-02-03 21:44:58 +0530

    Enemies Multiplayer Testing

--- Source/RPG/Private/Actor/RPGProjectile.cpp
+++ Source/RPG/Private/Actor/RPGProjectile.cpp
@@ -59,7 +59,7 @@ void ARPGProjectile::Destroyed()
 void ARPGProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
 	}
