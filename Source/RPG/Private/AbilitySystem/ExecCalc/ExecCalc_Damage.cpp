2d6a4cf48d8d628ce39af15b6b719d5558b50e81
Ansh Sharma <anshjangra15@gmail.com>
2026-02-05 16:12:10 +0530

    Enemies Final Polish

--- Source/RPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/RPG/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -107,7 +107,7 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 		checkf(RPGDamageStatics().TagsToCaptureDefs.Contains(ResistanceTag), TEXT("TagsToCaptureDefs doesn't contain Tag : [%s] in ExecCalc_Damage"), *ResistanceTag.ToString());
 		const FGameplayEffectAttributeCaptureDefinition CaptureDef = RPGDamageStatics().TagsToCaptureDefs[ResistanceTag];
 		
-		float DamageTypeValue = Spec.GetSetByCallerMagnitude(Pair.Key);
+		float DamageTypeValue = Spec.GetSetByCallerMagnitude(Pair.Key, false);
 		
 		float Resistance = 0.f;
 		ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(CaptureDef, EvaluationParameters, Resistance);
