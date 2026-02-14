e3c4865bb8276019d427183780ddd1b6fc50f1e9
Ansh Sharma <anshjangra15@gmail.com>
2026-02-14 16:31:34 +0530

    Ability Types

--- Source/RPG/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/RPG/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -55,6 +55,13 @@ void USpellMenuWidgetController::BindCallbacksToDependencies()
 
 void USpellMenuWidgetController::SpellGlobeSelected(const FGameplayTag& AbilityTag)
 {
+	if (bWaitingForEquipSelection)
+	{
+		const FGameplayTag SelectedAbilityType = AbilityInfo->FindAbilityInfoForTag(AbilityTag).AbilityType;
+		StopWaitingForEquipDelegate.Broadcast(SelectedAbilityType);
+		bWaitingForEquipSelection = false;
+	}	
+	
 	const FRPGGameplayTags GameplayTags = FRPGGameplayTags::Get();
 	const int32 SpellPoints = GetPS()->GetSpellPoints();
 	FGameplayTag AbilityStatus;
@@ -93,10 +100,25 @@ void USpellMenuWidgetController::SpendPointButtonPressed()
 
 void USpellMenuWidgetController::GlobeDeselect()
 {
+	if (bWaitingForEquipSelection)
+	{
+		const FGameplayTag SelectedAbilityType = AbilityInfo->FindAbilityInfoForTag(SelectedAbility.Ability).AbilityType;
+		StopWaitingForEquipDelegate.Broadcast(SelectedAbilityType);
+		bWaitingForEquipSelection = false;
+	}
+	
 	SelectedAbility.Ability = FRPGGameplayTags::Get().Abilities_None;
 	SelectedAbility.Status = FRPGGameplayTags::Get().Abilities_Status_Locked;
 	
 	SpellGlobeSelectedDelegate.Broadcast(false, false, FString(), FString());
+}
+
+void USpellMenuWidgetController::EquipButtonPressed()
+{
+	const FGameplayTag AbilityType = AbilityInfo->FindAbilityInfoForTag(SelectedAbility.Ability).AbilityType;
+	
+	WaitForEquipDelegate.Broadcast(AbilityType);
+	bWaitingForEquipSelection = true;
 }
 
 void USpellMenuWidgetController::ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton)
