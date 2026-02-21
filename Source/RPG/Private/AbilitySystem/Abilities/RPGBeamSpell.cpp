82e4e8d15b1033d8bbb6bf15327200ee2f43a50e
Ansh Sharma <anshjangra15@gmail.com>
2026-02-21 21:05:08 +0530

    First Trace Target

--- Source/RPG/Private/AbilitySystem/Abilities/RPGBeamSpell.cpp
+++ Source/RPG/Private/AbilitySystem/Abilities/RPGBeamSpell.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/Abilities/RPGBeamSpell.h"
 
 #include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void URPGBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
 {
@@ -25,5 +26,26 @@ void URPGBeamSpell::StoreOwnerVariables()
 	{
 		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
 		OwnerCharacter = Cast<ACharacter>(CurrentActorInfo->AvatarActor); 
+	}
+}
+
+void URPGBeamSpell::TraceFirstTarget(const FVector& BeamTargetLocation)
+{
+	check(OwnerCharacter);
+	if (OwnerCharacter->Implements<UCombatInterface>())
+	{
+		if (USkeletalMeshComponent* Weapon = ICombatInterface::Execute_GetWeapon(OwnerCharacter))
+		{
+			TArray<AActor*> ActorsToIgnore;
+			ActorsToIgnore.Add(OwnerCharacter);
+			FHitResult HitResult;
+			const FVector SocketLocation = Weapon->GetSocketLocation(FName("TipSocket"));
+			UKismetSystemLibrary::SphereTraceSingle(OwnerCharacter, SocketLocation, BeamTargetLocation, 10.f, TraceTypeQuery1, false, ActorsToIgnore, EDrawDebugTrace::None, HitResult, true);
+			if (HitResult.bBlockingHit)
+			{
+				MouseHitLocation = HitResult.ImpactPoint;
+				MouseHitActor = HitResult.GetActor();
+			}
+		}
 	}
 }
