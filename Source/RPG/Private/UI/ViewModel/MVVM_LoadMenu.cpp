d9164415d83a7e45ad263c1c245d7307cbd7fdda
Ansh Sharma <anshjangra15@gmail.com>
2026-03-02 03:21:03 +0530

    Switching the Widget Switcher

--- Source/RPG/Private/UI/ViewModel/MVVM_LoadMenu.cpp
+++ Source/RPG/Private/UI/ViewModel/MVVM_LoadMenu.cpp
@@ -18,7 +18,22 @@ void UMVVM_LoadMenu::InitializeLoadSlots()
 	
 }
 
-UMVVM_LoadSlot* UMVVM_LoadMenu::GetLoadSlotViewModelByIndex(int32 Index)
+UMVVM_LoadSlot* UMVVM_LoadMenu::GetLoadSlotViewModelByIndex(int32 Index) const
 {
 	return LoadSlots.FindChecked(Index);
+}
+
+void UMVVM_LoadMenu::NewSlotButtonPressed(int32 Slot, const FString& EnteredName)
+{
+	
+}
+
+void UMVVM_LoadMenu::NewGameButtonPressed(int32 Slot)
+{
+	LoadSlots[Slot]->SetWidgetSwitcherIndex.Broadcast(1);
+}
+
+void UMVVM_LoadMenu::SelectSlotButtonPressed(int32 Slot)
+{
+	
 }
